d96cf89f754daca06f1cfe6dca04aed31d3d78eb
kevin578 <kevinbriggs1@gmail.com>
2016-03-27 21:25:14 -0400

    pre JSON

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,35 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var formattedName;
+var formattedRole;
+
+formattedName = HTMLheaderName.replace("%data%","Kevin Briggs");
+formattedRole = HTMLheaderRole.replace("%data%","Teacher");
+
+
+var bio = {
+	name:"Kevin",
+	role: "teacher",
+	contact: "kevin@gmail.com"
+};
+
+
+var work = {
+	position: "Technology",
+	employer: "Bancroft",
+	years: 3
+};
+
+var education = {
+	name: "La Salle",
+	years: 4,
+	city: "Philadelphia"
+};
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#main").append(bio.name);
+$("#main").append(work["position"]);
+$("#main").append(education.name);
+
+
+
